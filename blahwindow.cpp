50accc399fd24a80b8ba619d6a16492029696eaf
Price Bryan <pbryan422@gmail.com>
2017-02-07 17:09:14 +0000

    random

---
+++ blahwindow.cpp
@@ -0,0 +1,17 @@
+#include <SDL.h>
+
+int main(int argc, char* args[])
+{
+    SDL_Window* window = NULL;
+    
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    window = SDL_CreateWindow("BLAH WINDOW", 0, 0, 1920, 975, SDL_WINDOW_SHOWN || SDL_WINDOW_RESIZABLE);
+    
+    SDL_Delay(4000);
+    
+    SDL_DestroyWindow(window);
+    
+    return 1;
+    
+}
